99e57998d8e46e007a240c4ff80cae591399e861
David <dvdlee41@gmail.com>
2020-11-07 21:33:16 +0800

    Adding rightBarButtonItem for showing row animation

--- ExpandableTableView/ViewController.swift
+++ ExpandableTableView/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UITableViewController {
+  // MARK: - Property
   let cellId = "cellId"
   
   let names = [
@@ -30,8 +31,13 @@ class ViewController: UITableViewController {
     ["Patrick", "Patty"]
   ]
   
+  var showIndexPaths = false
+  
+  // MARK: - Life cycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Show IndexPath", style: .plain, target: self, action: #selector(handleShowIndexPath))
     
     navigationItem.title = "Contacts"
     navigationController?.navigationBar.prefersLargeTitles = true
@@ -39,6 +45,28 @@ class ViewController: UITableViewController {
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
   }
   
+  // MARK: - Selector Methods
+  @objc func handleShowIndexPath() {
+    print("Attemping reload animation of indexPaths...")
+    
+    var indexPathsToReload = [IndexPath]()
+    
+    for section in twoDimensionalArray.indices {
+      for row in twoDimensionalArray[section].indices {
+        print(section, row)
+        let indexPath = IndexPath(row: row, section: section)
+        indexPathsToReload.append(indexPath)
+      }
+    }
+    
+    showIndexPaths = !showIndexPaths
+    
+    let animationStyle = showIndexPaths ? UITableView.RowAnimation.right : UITableView.RowAnimation.left
+    
+    tableView.reloadRows(at: indexPathsToReload, with: animationStyle)
+  }
+  
+  // MARK: - UITableView Delegate Methods
   override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
     let label = UILabel()
     label.text = "Header"
@@ -46,6 +74,7 @@ class ViewController: UITableViewController {
     return label
   }
   
+  // MARK: - UITableView DataSource Methods
   override func numberOfSections(in tableView: UITableView) -> Int {
     return twoDimensionalArray.count
   }
@@ -58,6 +87,10 @@ class ViewController: UITableViewController {
     let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
     let name = twoDimensionalArray[indexPath.section][indexPath.row]
     cell.textLabel?.text = name
+    
+    if showIndexPaths {
+      cell.textLabel?.text = "\(name) Section:\(indexPath.section) Row:\(indexPath.row)"
+    }
     return cell
   }
 }
