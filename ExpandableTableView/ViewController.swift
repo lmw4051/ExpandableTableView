da6452fcbf8d5a382eb5425dfa5e1b36c220c43e
David <dvdlee41@gmail.com>
2020-11-07 20:46:02 +0800

    . Adding data for UITableView . Register UITableViewCell . Populate Data

--- ExpandableTableView/ViewController.swift
+++ ExpandableTableView/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+  let cellId = "cellId"
+  
+  let names = [
+    "Amy", "Bill", "Zack", "Steve", "Jack"
+  ]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     navigationItem.title = "Contacts"
     navigationController?.navigationBar.prefersLargeTitles = true
+    
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return names.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+    let name = self.names[indexPath.row]
+    cell.textLabel?.text = name
+    return cell
   }
 }
-
